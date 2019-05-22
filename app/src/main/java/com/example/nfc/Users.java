ab9a4666354b1d64069ea85b1a26e1194188944a
dany <dany.c23@ua.pt>
2019-05-22 17:21:11 +0100

    Treinador e LogIn  e Create Acount

---
+++ app/src/main/java/com/example/nfc/Users.java
@@ -0,0 +1,8 @@
+public class Users {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+}
